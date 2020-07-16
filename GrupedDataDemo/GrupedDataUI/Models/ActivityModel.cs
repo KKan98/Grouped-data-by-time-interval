40aef0ef7bde15380a75c2610549123a4d85aaca
KKan98 <48765813+KKan98@users.noreply.github.com>
2020-07-16 15:38:56 +0200

    Add actievieties

---
+++ GrupedDataDemo/GrupedDataUI/Models/ActivityModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrupedDataUI.Models
+{
+    public class ActivityModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Enter the activity")]
+        public string Activity { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Date { get; set; }
+    }
+}
